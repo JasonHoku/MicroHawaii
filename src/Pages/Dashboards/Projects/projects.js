6d0713fc9e7f45b9c23dfcd46f69cf0aff58e183
unknown <jlevien808@gmail.com>
2020-11-05 00:36:35 -1000

    Timer Script Improve | Projects Page

--- src/Pages/Dashboards/Projects/projects.js
+++ src/Pages/Dashboards/Projects/projects.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from "react";
+import React, { Component, Fragment, useState } from "react";
 import scriptLoader from "react-async-script-loader";
 import CSSTransitionGroup from "react-transition-group/CSSTransitionGroup";
 import classnames from "classnames";
@@ -17,6 +17,7 @@ import {
   Nav,
   NavItem,
   ListGroup,
+  Input,
   ListGroupItem,
   Card,
   CardBody,
@@ -62,6 +63,8 @@ import avatar3 from "../../../assets/utils/images/avatars/3.jpg";
 import contrastus from "../../../assets/images/contrastus.png";
 import collage from "../../../assets/images/collage.png";
 import mandalashirt from "../../../assets/images/mandalashirt.png";
+import { toInteger } from "lodash";
+import { relative } from "path";
 
 const CLIENT = {
   sandbox: process.env.PAYPAL_CLIENT_ID_SANDBOX,
@@ -171,9 +174,11 @@ export default class ProjectElements extends Component {
 
     this.timerLoader = this.timerLoader.bind(this);
     this.toggle2 = this.toggle2.bind(this);
+    this.timerReset = this.timerReset.bind(this);
     this.state = {
       activeTab2: "222",
       activeTab1: "11",
+      timerVar: 1200,
       data: makeData(),
     };
   }
@@ -195,9 +200,18 @@ export default class ProjectElements extends Component {
   }
   timerLoader() {
     document.getElementById("timer").hidden = false;
+    this.setState({});
+  }
+  timerReset() {
+    if ( toInteger(document.getElementById("timerText").value) === 0) {document.getElementById("timerText").value = 333 }
+
+    this.state.timerVar = toInteger(document.getElementById("timerText").value);
+    this.setState({});
   }
   render() {
     const { data } = this.state;
+    let text;
+    let timerVar = this.state.timerVar;
 
     return (
       <Fragment>
@@ -209,11 +223,17 @@ export default class ProjectElements extends Component {
           transitionEnter={false}
           transitionLeave={false}
         >
-          <Row>
+          <Row
+            style={{
+              alignContent: "center",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
             <Col xs="auto" sm="auto" md="auto" xl="auto">
               <Card>
                 <CardBody>
-                 Welcome to microHawaii projects portfolio!
+                  Welcome to microHawaii projects blog!
                   <br></br>
                 </CardBody>
               </Card>
@@ -221,11 +241,18 @@ export default class ProjectElements extends Component {
           </Row>
           <br></br>
 
-          <Row>
+          <Row
+            style={{
+              alignContent: "center",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
             <Col xs="auto" sm="auto" md="auto" xl="auto">
               <Card>
                 <CardBody>
-                  A variety of blogs and projects will become more available here over the coming weeks.
+                  A variety of articles and projects will become more available
+                  here over the coming weeks.
                   <br /> <br />
                   <li>
                     Website Development: Follow on
@@ -242,12 +269,89 @@ export default class ProjectElements extends Component {
                   <br></br>{" "}
                   <li>
                     <a href="/#/dashboards/projects" onClick={this.timerLoader}>
-                      Timer Script (for Ani)
+                      ⏱Timer Script (for Ani)
                     </a>
                   </li>
                   <br></br>
                 </CardBody>
               </Card>
+              <Row
+                id="timer"
+                hidden="true"
+                style={{
+                  alignContent: "center",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                {" "}
+                <Card style={{ fontSize: "40px", width: "26rem" }}>
+                  {" "}
+                  <Row>
+                    <CardBody>
+                      <p>
+                        <a
+                          onClick={this.timerReset}
+                          href="/#/dashboards/projects"
+                        >
+                          <i className="pe-7s-plus"></i>
+                        </a>
+                        <a>
+                          <i className="pe-7s-less"></i>
+                          <i className="pe-7s-lock"></i>
+                          <i className="pe-7s-refresh"></i>
+                        </a>
+                      </p>
+                      <Input
+                        style={{
+                          width: "60%",
+                          alignContent: "center",
+                          alignItems: "center",
+                          position: "relative",
+                          display: "block",
+                          justifyContent: "center",
+                        }}
+                        width="13rem"
+                        size="10"
+                        type="text"
+                        id="timerText"
+                        placeholder={timerVar}
+                      >
+                        {" "}
+                      </Input>{" "}
+                    </CardBody>
+                    <br></br>
+                    <Col>
+                      <CountdownCircleTimer
+                        isPlaying={true}
+                        duration={this.state.timerVar}
+                        key={this.state.timerVar}
+                        colors={[
+                          ["#004777", 0.33],
+                          ["#F7B801", 0.33],
+                          ["#A30000", 0.33],
+                        ]}
+                      >
+                        {({ remainingTime }) => remainingTime}
+                      </CountdownCircleTimer>
+                    </Col>
+                    <Col>
+                      <CountdownCircleTimer
+                        isPlaying
+                        duration={120}
+                        colors={[
+                          ["#004777", 0.33],
+                          ["#F7B801", 0.33],
+                          ["#A30000", 0.33],
+                        ]}
+                      >
+                        {({ remainingTime }) => remainingTime}
+                      </CountdownCircleTimer>
+                    </Col>
+                  </Row>
+                </Card>
+                <Col xs="auto" sm="auto" md="auto" xl="auto"></Col>
+              </Row>
             </Col>
           </Row>
           <br></br>
@@ -257,6 +361,7 @@ export default class ProjectElements extends Component {
               width: "60%",
               alignContent: "center",
               alignItems: "center",
+              justifyContent: "center",
             }}
           >
             <CardHeader>
@@ -273,46 +378,6 @@ export default class ProjectElements extends Component {
             </CardBody>
           </Card>
           <br />
-          <Row id="timer" hidden="true">
-            <Col xs="auto" sm="auto" md="auto" xl="auto">
-              <Card>
-                <CardHeader>20Minutes:</CardHeader>
-                <CardBody>
-                  <CountdownCircleTimer
-                    isPlaying
-                    duration={1200}
-                    colors={[
-                      ["#004777", 0.33],
-                      ["#F7B801", 0.33],
-                      ["#A30000", 0.33],
-                    ]}
-                  >
-                    {({ remainingTime }) => remainingTime}
-                  </CountdownCircleTimer>
-                </CardBody>
-              </Card>
-            </Col>
-            <Col xs="auto" sm="auto" md="auto" xl="auto">
-              <Card>
-                <CardHeader>2Minutes:</CardHeader>{" "}
-                <CardBody>
-                  {" "}
-                  <CountdownCircleTimer
-                    isPlaying
-                    duration={120}
-                    colors={[
-                      ["#004777", 0.33],
-                      ["#F7B801", 0.33],
-                      ["#A30000", 0.33],
-                    ]}
-                  >
-                    {({ remainingTime }) => remainingTime}
-                  </CountdownCircleTimer>
-                </CardBody>
-              </Card>
-            </Col>
-          </Row>
-          <br></br>
 
           <Row>
             <Col xs="auto" sm="auto" md="auto" xl="auto">
