23ba10666bb5b87953dcf081e2d137bea8c99505
JasonHoku <jlevien808@gmail.com>
2020-12-14 08:21:40 -1000

    Package Updates

--- src/Layout/AppHeader/Components/analytics.js
+++ src/Layout/AppHeader/Components/analytics.js
@@ -4,7 +4,6 @@ import ReactGA from "react-ga";
 
 ReactGA.initialize("UA-102481694-7");
 ReactGA.pageview(window.location + window.location.search);
-ReactGA.ga;
 function sendToGoogleAnalytics({ name, delta, id }) {
   // Assumes the global `ga()` function exists, see:
   // https://developers.google.com/analytics/devguides/collection/analyticsjs
@@ -40,22 +39,6 @@ class SendToGoogleAnalytics extends React.Component {
     };
   }
   render() {
-    ReactGA.ga("send", "event", {
-      eventCategory: "Web Vitals",
-      eventAction: name,
-      // Google Analytics metrics must be integers, so the value is rounded.
-      // For CLS the value is first multiplied by 1000 for greater precision
-      // (note: increase the multiplier for greater precision if needed).
-      // The `id` value will be unique to the current page load. When sending
-      // multiple values from the same page (e.g. for CLS), Google Analytics can
-      // compute a total by grouping on this ID (note: requires `eventLabel` to
-      // be a dimension in your report).
-      // Use a non-interaction event to avoid affecting bounce rate.
-      nonInteraction: true,
-      // Use `sendBeacon()` if the browser supports it.
-      transport: "beacon",
-    });
-
     return null;
   }
 }
